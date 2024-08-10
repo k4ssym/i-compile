bce5a562abe67d944278e3d574779386edfda6d2
k4ssym <zhomartqasym@gmail.com>
2024-08-10 16:46:41 +0500

    Updated PostgreSQL connection string to use the external database URL

--- models/index.js
+++ models/index.js
@@ -1,9 +1,12 @@
-// models/index.js
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize('users', 'postgres', '.nIs08nIs.', {
-    host: 'localhost',
+// Use the External Database URL for the connection
+const sequelize = new Sequelize('postgresql://users_wdyn_user:qrv6rlo2yqNmeYTUUUZzWOPSrLvKv66z@dpg-cqrl2f0gph6c73a13np0-a.oregon-postgres.render.com/users_wdyn', {
     dialect: 'postgres',
+    ssl: {
+        require: true,
+        rejectUnauthorized: false // This is important if you are connecting to a hosted database
+    }
 });
 
 export default sequelize;
